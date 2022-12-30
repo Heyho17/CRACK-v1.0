241f730e18889b320bdc929c6d4fc15f45e89de9
ClassyBastardCyber <117260932+Heyho17@users.noreply.github.com>
2022-12-30 23:01:45 +0700

    Add files via upload

---
+++ CRACK.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+#version 1.0
+
+#MAU NGAPAIN TOT?
+#RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#MALU TOLOL!!!
+#KAGA BERMORAL!
+#GUA DOA'IN YG COPYRIGHT COPAS RECORD TANPA IDZIN
+#BISUL GK SEMBUH SEMBUH.
+#AAMIIN
+#BIKIN SCRIPT INI PUYENG ANJING
+#GAUSAH ASAL RECORDE COPYRIGHT COPAS TANPA IDZIN KE GUA!!
+#MASIH MENDING GUA KASIH GRATISAN LU MONYET!
+#KALO MAU COPYRIGHT COPAS RECORD EDIT AUTHOR
+#IDZIN DULU KE FACEBOOK ADIK GUA!
+#MORE INFO DM FACEBOOK : ANISSA SYAH
+
+#17/08/18
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+clear
+# tampilan
+clear
+toilet -f small -F gay Crack Facebook
+echo "════════════════════════════════════════════════════════════════════" | lolcat
+echo "====================================================================" | lolcat
+echo "═══════════════════════.:^7YPGB##&&&&&&&&&&&&&##BBG5Y?~.▁█▆▂▃▄▆▁▂▃██" | lolcat
+echo "═════════════════════:!YPGBB@@@@@@@@@@@@@@@@@@@@@@@@@@@@P▁█▆▂▃▄▆▁▂▃█" | lolcat
+echo "═════════════════════7GBBBBBGB@@@@@@@@@@@@@@@@@@@@@@@@@@@@B▁█▆▂▃▄▆▁▂" | lolcat
+echo "════════════════════:BBGGGGB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@~▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════!BGGBGB#&&@@@@@@@@@@@@@@@@@@&&##&&@@@@@B▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════JGG5J!~^^~7JP#@@@@@@@@@@&G!:.:~!77?P@@@@▁█▆▂▃▄▆▃" | lolcat
+echo "════════════════════5GJYB&##GY!: ^G@@@@@@B^ .!G&@@@@@@BB@@@@▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════5PPGG@@@@@@@#?.^@@@@@@J!G@@@@@@@@@@@@@@@▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════5GPP##GPPPG#&@#GB&@@@@@@@&B5?7!7JG&@@@@@▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════5PPB&?^:...:~JGPPP@@@@@@&Y!~~~!7?5&@@@@@▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════55P#@@@&&&&&@#PPPP&@@@@@@@@@@@@@@@@@@@@@▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════YYPB@@@@@@@@@GPPPP@@@@@@@@@@@@@@@@@@@@@?▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════?JPP@@@@@@@#P5PP5G@@@@@@@@@@@@@@@@@@@&YY▁█▆▂▃▄▆▁" | lolcat
+echo "════════════════════:YJYB@@@@BPY55555B@@@@@@@@@@@@@@@@@@&#@:▁█▆▂▃▄▆▁" | lolcat
+echo "═════════════════════^5YJ5PJPYY555P5YP@@@#BB@@@@@@&P~G#B@@&▁█▆▃▄▆▁▂▃" | lolcat
+echo "═════════════════════:5555..?5555P5~ . :G@@@@@&J J@#&&@?@;;▁█▆▂▃▄▃██" | lolcat
+echo "══════════════════════:55G&J...... ?HEYHO. .~~~^:~#@#&&@&:▁█▆▂▃▄▆▁▂▃" | lolcat
+echo "═══════════════════════.Y5P#&#BPY?7?G&&&BYJYY5B#&@@#&&@@:▁█▆▂▃▄▆▁▂▃█" | lolcat
+echo "                        .J5YYPB#&&@@&&&&&&@@@@@@@&&@&@#.▁█▆▂▃▄▆▁▂▃██" | lolcat
+echo "     DON'T WORY BE       75YYYYY5&@&7^^~#@@@@@@&&@@&7▁█▆▂▃▄▆▁▂▃████▅" | lolcat
+echo "         HAPPY            :JYYYYJ#@&~ ^@@@@@@@&@@&YY▁█▆▂▃▄▆▁▂▃████▅▂" | lolcat
+echo "      ===========           ^?YY5GGJ B@@@@@@&Y..@@;▁█▆▂▃▄▆▁▂▃████▅▂▂" | lolcat
+echo "    GUNAKAN SCRIPT INI        .!YB&G. &@@@@&Y.@YY;▁█▆▂▃▄▆▁▂██▅▂▂▃▄▅▇" | lolcat
+echo "      DENGAN BIJAK!!             :?57 !@@&B!Y.;; ▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "═══════════════════════════════════.. ^J~......  ▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "═════════════════════════════════════/HEYHO\▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄▅▇▆▁▂" | lolcat
+echo "════════════════════════╔═══╗╔═══╗╔═══╗╔═══╗╔╗╔═╗ ▁█▆▂▃▄▆▁▂▃████▅▂▂▃" | lolcat
+echo "════════════════════════║╔═╗║║╔═╗║║╔═╗║║╔═╗║║║║╔╝▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "════════════════════════║║─╚╝║╚═╝║║║─║║║║─╚╝║╚╝╝─▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "════════════════════════║║─╔╗║╔╗╔╝║╚═╝║║║─╔╗║╔╗║─▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "══════════════          ║╚═╝║║║║╚╗║╔═╗║║╚═╝║║║║╚╗▁█▆▂▃▄▆▁▂▃████▅▂▂▃▄" | lolcat
+echo "════════════════════════╚═══╝╚╝╚═╝╚╝─╚╝╚═══╝╚╝╚═╝                   " | lolcat
+echo "[•]TYPE     : TOOLS CRACK                                      [•]" | lolcat
+echo "[•]VERSION  : V.0                                              [•]" | lolcat
+echo "[•]AUTHOR   : MR.HEYHO                                         [•]" | lolcat
+echo "[•]FACEBOOK : ANISSA SYAHPUTRI                                 [•]" | lolcat
+echo "[•]YOUTUBE  : HEYHO-CYBER                                      [•]" | lolcat
+echo "[•]TEAM     : CLASSY BASTARD CYBER                             [•]" | lolcat
+echo "[•]____________________________________________________________[•]" | lolcat
+sleep 2
+echo    [•]"[SELAMAT MENCOBA]  ║" | lolcat
+echo    [•]"═══════════════════╝" | lolcat
+sleep 3
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> TOOLS NU SIA INSTALL AYA DI DIRECTORY TOOLS AINK " |lolcat
+echo -b $green"[#]> Thanks For Using My Tools" |lolcat
+sleep 1
+echo -b $green"[#]> MR.HEYHO On Your System" |lolcat
+
+echo -b $green"[#]> See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+figlet MR.HEYHO | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""
+echo ""
+
+echo "######################################" | lolcat
+echo "@              WELCOME               @" | lolcat
+echo "######################################" | lolcat
+echo "╔═══════════════════════════════════════════════════════════════╗" | lolcat
+echo "║                           MENU                                ║" | lolcat
+echo "╚═══════════════════════════════════════════════════════════════╝" | lolcat
+echo "                ┃                  " | lolcat
+echo " ╔════════════════════════════════╗" | lolcat
+echo " ║          ╚► NOTE ◄╝            ║" | lolcat
+echo " ║    SILAHKAN LOGIN FACEBOOK     ║" | lolcat
+echo " ║  UNTUK MENDAPATKAN AKSES TOKEN ║" | lolcat
+echo " ╚════════════════════════════════╝" | lolcat
+echo "     ┃                      ┃            " | lolcat
+echo "╔═══════════╗      ╔════════════════════╗" | lolcat
+echo "║╚►1.CRACK  ║      ║╚►2.DAPATKAN TOKEN  ║" | lolcat
+echo "╚═══════════╝      ╚════════════════════╝" | lolcat
+echo "     ╚══╗           ╔══╝                 " | lolcat
+echo "     ╔═════════════════╗                 " | lolcat
+echo "     ║╚►00.Exit        ║                 " | lolcat
+echo "     ╚═════════════════╝                 " | lolcat
+echo    "╭─[PILIH NOMORNYA]                    " | lolcat
+read -p "   ╰─root@./MR.HEYHO=" pil;
+clear
+sleep 2
+
+# CRACK
+
+case $pil in
+1) echo "═════════════════════════════════════════════" | lolcat
+echo "       ░▐█▀█▒▐█▀▀▄░░▄█▀▄─░▐█▀█▒▐█▒▐▀         " | lolcat
+echo "       ░▐█──▒▐█▒▐█░▐█▄▄▐█░▐█──▒▐██▌░         " | lolcat
+echo "       ░▐█▄█▒▐█▀▄▄░▐█─░▐█░▐█▄█▒▐█▒▐▄         " | lolcat
+echo "          ════════════════════════════════   " | lolcat
+        read -p "[•]Token fb : " pass
+echo $pass
+sleep 1
+echo
+if [ $pass = "SILAHKAN LOGIN FACEBOOK" ]
+        then
+        echo -e $g   [•]"ACCESS RECEIVED"
+        sleep 2
+clear
+        else
+        echo [•]" AKSES DI TOLAK" | lolcat
+        sleep 2
+        echo [•]" LOGIN FACEBOOK UNTUK MENDAPATKAN TOKEN "
+        sleep 3
+        echo [•]" THANKS YOU" | lolcat
+        sleep 4
+        sh CRACK.sh
+        sleep 5
+fi
+sleep 2
+
+;;
+
+
+# LOGIN FACEBOOK
+
+2) echo "LOGIN FACEBOOK DULU YH KAWAN 👍" | lolcat
+   echo "UNTUK AMBIL AKSES TOKEN NYA 🎭"
+   echo "BARU DEH MULAI HACK SESUKA KALIAN" | lolcat
+   sleep 6
+
+xdg-open http://127.0.0.1:8080
+sleep 2
+
+;;
+
+
+00) echo "AUTHOR: MR.HEYHO" | lolcat
+echo "MR.HEYHO ON YOUR SYSTEM" | lolcat
+echo "CLASSY BASTARD CYBER" | lolcat
+echo "JAMGAN DI SALAH GUNAKAN 🙏" | lolcat
+echo "KAMI TIDAK BERTANGGUNG JAWAB" | lolcat
+echo "JIKA PENGGUNA SCRIPT INI MENYALAH GUNAKAN"
+figlet MR.HEYHO | lolcat
+exit
+;;
+
+*) echo "Sorry, Your choices it's not already [T4T]"
+esac
+done
+done
+
+#[081221215191]
+#[MR.HEYHO]
+fi
+sleep 1
